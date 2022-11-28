8ba4246ae1ec798ffa3863910230a3a288a16e01
huntersteffner <huntersteffner@gmail.com>
2022-11-28 10:47:00 -0500

    Added tailwind

---
+++ gym-website/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold bg-black text-white underline">
+      Hello world!
+    </h1>
+  )
+}
