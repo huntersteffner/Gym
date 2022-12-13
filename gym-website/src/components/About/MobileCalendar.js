775fad829f5651bd2a7c32aba8d1e1c202062f8a
huntersteffner <huntersteffner@gmail.com>
2022-12-13 15:08:00 -0500

    Mobile Calendar Template Work

--- gym-website/src/components/About/MobileCalendar.js
+++ gym-website/src/components/About/MobileCalendar.js
@@ -1,14 +1,17 @@
+import { useState } from 'react'
+
 const MobileCalendar = () => {
+  const [dayOfWeek, setDayOfWeek] = useState('Monday')
 
-    const weekHandler = () => {
-
-    }
+  const weekHandler = (e) => {
+    setDayOfWeek(e.target.value)
+  }
   return (
     <div className="container mx-auto mt-10">
       <div className="wrapper bg-white rounded shadow w-full">
         <div className="header flex justify-center space-x-3 border-b p-2">
           <span>Select Day</span>
-          <select name="days" id="days">
+          <select onChange={weekHandler} name="days" id="days">
             <option value="Monday">Monday</option>
             <option value="Tuesday">Tuesday</option>
             <option value="Wednesday">Wednesday</option>
@@ -22,33 +25,33 @@ const MobileCalendar = () => {
         <div className="w-full">
           <div className="flex justify-center p-2 border-r h-10">
             <h2>
-              <span id="day-of-week"></span>'s Classes
+              <span id="day-of-week">{dayOfWeek}</span>'s Classes
             </h2>
           </div>
         </div>
         {/* List of classes */}
-        <div>
-          <div className="flex justify-between items-center border py-3 px-4 cursor-pointer">
+        <ul className="mobile-calendar">
+          <li className="flex justify-between items-center border py-3 px-4 cursor-pointer odd:bg-gray-100">
             <h1>Class A</h1>
             <h1>8-9 AM</h1>
-          </div>
-          <div className="flex justify-between items-center border py-3 px-4 cursor-pointer">
+          </li>
+          <li className="flex justify-between items-center border py-3 px-4 cursor-pointer odd:bg-gray-100">
             <h1>Class B</h1>
             <h1>8-9 AM</h1>
-          </div>
-          <div className="flex justify-between items-center border py-3 px-4 cursor-pointer">
+          </li>
+          <li className="flex justify-between items-center border py-3 px-4 cursor-pointer odd:bg-gray-100">
             <h1>Class C</h1>
             <h1>8-9 AM</h1>
-          </div>
-          <div className="flex justify-between items-center border py-3 px-4 cursor-pointer">
+          </li>
+          <li className="flex justify-between items-center border py-3 px-4 cursor-pointer odd:bg-gray-100">
             <h1>Class D</h1>
             <h1>8-9 AM</h1>
-          </div>
-          <div className="flex justify-between items-center border py-3 px-4 cursor-pointer">
+          </li>
+          <li className="flex justify-between items-center border py-3 px-4 cursor-pointer odd:bg-gray-100">
             <h1>Class E</h1>
             <h1>8-9 AM</h1>
-          </div>
-        </div>
+          </li>
+        </ul>
       </div>
     </div>
   )
