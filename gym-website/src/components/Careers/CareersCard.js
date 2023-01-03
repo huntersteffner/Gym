02a305252e6007180abdec03b8cdc5d1c63c60f4
huntersteffner <huntersteffner@gmail.com>
2023-01-03 11:05:04 -0500

    Careers page changes

--- gym-website/src/components/Careers/CareersCard.js
+++ gym-website/src/components/Careers/CareersCard.js
@@ -1,20 +1,28 @@
-const CareersCard = ({image, title, text}) => {
-    return (
-        <div className="bg-blue-100 w-2/3">
-          <img src={image} alt="" />
-          <div className='m-4'>
-            <h3 className='font-bold text-2xl'>{title}</h3>
-            <p className='text-lg'>
-              {text}
-            </p>
-            <div className='flex justify-between items-center w-full'>
-              <label className='w-[15rem]' >Add your Resume</label>
-              <input  type="file" />
-            </div>
-            <button className="bg-red-300 rounded-xl w-full py-2">Submit</button>
-          </div>
+const CareersCard = ({ image, title, text, responsibilities }) => {
+  return (
+    <div className="bg-blue-100 w-2/3">
+      <img src={image} alt="Job" />
+      <div className="m-4">
+        <h3 className="font-bold text-2xl">{title}</h3>
+        <div className="text-lg">
+          <p>{text}</p>
+          <p className="underline">Responsabilities Include</p>
+          <ul>
+            {responsibilities.map((responsability) => (
+              <li className="ml-3" key={responsability.key}>
+                {responsability.li}
+              </li>
+            ))}
+          </ul>
         </div>
-    )
+        <div className="flex justify-between items-center w-full">
+          <label className="w-[15rem]">Add your Resume</label>
+          <input type="file" />
+        </div>
+        <button className="bg-red-300 rounded-xl w-full py-2">Submit</button>
+      </div>
+    </div>
+  )
 }
 
 export default CareersCard
--- gym-website/src/pages/Careers.js
+++ gym-website/src/pages/Careers.js
@@ -1,10 +1,85 @@
+import { useState } from 'react'
+
 import BannerText from '../components/BannerText'
 import CareersCard from '../components/Careers/CareersCard'
 
 // https://unsplash.com/photos/nlZTjUZX2qo
 import teamMember from '../img/careers-team-member.jpg'
+import personalTrainer from '../img/personal-training-woman-ropes.jpg'
+import fitnessInstructor from '../img/personal-training-cardio.jpg'
+import yogaInstructor from '../img/personal-training-yoga.jpg'
+
 
 const Careers = () => {
+  
+  const jobs = {
+    teamMember: {
+      title: 'Team Member',
+      text: `Become one of our team members and assist with all the day to day jobs and responsibilities of maintaining the gym.`,
+      responsibilities: [
+        {li: 'Greeting Members at Front Desk', key: 1},
+        {li:'Registering New Members', key: 2},
+        {li:'Cleaning the Facility', key: 3},
+        {li:'Organizing Equipment', key: 4},
+        {li: 'Guarantee Safe Environment for all Members', key: 5}
+      ],
+      image: teamMember
+    },
+    personalTrainer: {
+      title: 'Personal Trainer',
+      text: 'Help guide others on their fitness journey with competitive compensation.',
+      responsibilities: [
+        {li: 'Form one-on-one relationships with Trainees', key: 1},
+        {li:'Identify unique fitness needs and goals to each Trainee', key: 2},
+        {li:'Create tailored workout plans to individuals\'s needs', key: 3},
+        {li:'Correct and improve form on individuals during workouts', key: 4},
+        {li: 'Track trainee\'s progress over time', key: 5},
+        {li: 'Monitor that safety regulations are being followed', key: 6}
+      ],
+      image: personalTrainer
+    },
+    fitnessInstructor: {
+      title: 'Fitness Instructor',
+      text: 'Lead our group fitness classes',
+      responsibilities: [
+        {li: 'Lead HIIT and Cardio Group Classes', key: 1},
+        {li:'Bring encouraging attitude', key: 2},
+        {li:'Facilitate comradery among members participating', key: 3},
+        {li:'Monitor that safety regulations are being followed', key: 4}
+      ],
+      image: fitnessInstructor
+    },
+    yogaInstructor: {
+      title: 'Yoga Instructor',
+      text: 'Lead our yogo classes to assist in recovery and relaxation',
+      responsibilities: [
+        {li: 'Lead yoga classes', key: 1},
+        {li:'Instruct members in correct yoga movements', key: 2},
+        {li:'Monitor that safety regulations are being followed', key: 3},
+        {li:'Organizing Equipment', key: 4},
+        {li: 'Guarantee Safe Environment for all Members', key: 5}
+      ],
+      image: yogaInstructor
+    }
+  }
+
+  const [job, setJob] = useState({
+    title: jobs.teamMember.title,
+    text: jobs.teamMember.text,
+    image: jobs.teamMember.image,
+    responsibilities: jobs.teamMember.responsibilities
+  })
+
+  const jobHandler = (e) => {
+    setJob({
+      title: jobs[e.target.id].title,
+      text: jobs[e.target.id].text,
+      image: jobs[e.target.id].image,
+      responsibilities: jobs[e.target.id].responsibilities
+    })
+  }
+
+
   return (
     <>
       {/* https://unsplash.com/photos/CPSjcuuV8E8 */}
@@ -18,13 +93,13 @@ const Careers = () => {
       <section className="container mx-auto flex justify-between">
         <div>
           <h2>Open Positions</h2>
-          <div className='text-2xl'>Team Member</div>
-          <div>Personal Trainer</div>
-          <div>Fitness Instructor</div>
-          <div>Yoga Instructor</div>
+          <div id='teamMember' className='text-2xl' onClick={jobHandler}>Team Member</div>
+          <div id='personalTrainer' className='text-2xl' onClick={jobHandler}>Personal Trainer</div>
+          <div id='fitnessInstructor' className='text-2xl' onClick={jobHandler}>Fitness Instructor</div>
+          <div id='yogaInstructor' className='text-2xl' onClick={jobHandler}>Yoga Instructor</div>
           
         </div>
-        <CareersCard image={teamMember} title='Job title' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aliquid, eveniet ipsum placeat architecto fugit laudantium atque sapiente? Ducimus quidem doloribus sequi fugit incidunt eos dolore laborum eum reiciendis aliquam quae asperiores nulla pariatur nisi molestias ad odit quisquam temporibus, repudiandae libero aspernatur labore quis, velit suscipit? Aut, quaerat ea?'/>
+        <CareersCard image={job.image} title={job.title} text={job.text}responsibilities={job.responsibilities}/>
       </section>
     </>
   )
