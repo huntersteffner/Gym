99f00960395035bc203c69ce3fbce601ead4b654
huntersteffner <huntersteffner@gmail.com>
2023-01-04 09:46:42 -0500

    Finished membership form

--- gym-website/src/pages/Membership.js
+++ gym-website/src/pages/Membership.js
@@ -52,25 +52,39 @@ const Membership = () => {
         </div>
       </section>
       <section className="bg-amber-800 text-white" id="membership-form">
-        <div className='container mx-auto'>
-            <p className='text-xl text-center '>We'd love to have you as a part of our gym community. We want all our potential members to fully understand what our gym has to offer before signing up. Schedule a tour and come on by to see what we have to offer.</p>
+        <div className="container mx-auto text-center">
+          <h4 className="text-2xl font-bold">Become a member today!</h4>
+          <p className="text-xl ">
+            We'd love to have you as a part of our gym community. We want all
+            our potential members to fully understand what our gym has to offer
+            before signing up. Schedule a tour and come on by to see what we
+            have to offer.
+          </p>
         </div>
-        <form className="flex flex-col justify-center items-center container mx-auto space-y-2">
-          <div className='flex flex-col justify-center md:flex-row'>
-            <div className="flex flex-col">
-              <div>
-                <input type="text" name="name" id="name" placeholder="Name" />
-              </div>
-              <div>
+        <form className="flex flex-col justify-center items-center container mx-auto p-2">
+          <div className="flex flex-col justify-center w-full space-y-2 md:flex-row md:space-y-0 md:space-x-2">
+            <div className="flex flex-col space-y-2 w-full md:w-auto">
+              <div className="w-full">
                 <input
+                  className="w-full"
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Name"
+                />
+              </div>
+              <div className="w-full">
+                <input
+                  className="w-full"
                   type="email"
                   name="email"
                   id="email"
                   placeholder="Email"
                 />
               </div>
-              <div>
+              <div className="w-full">
                 <input
+                  className="w-full"
                   type="text"
                   name="phone"
                   id="phone"
@@ -78,34 +92,48 @@ const Membership = () => {
                 />
               </div>
             </div>
-            <div>
+            <div className="w-full md:w-auto">
               <div>
                 <textarea
-                className='h-20'
+                  className="h-20 w-full"
                   type="text"
                   name="address"
                   id="address"
                   placeholder="Address"
                 />
               </div>
-              <div>
-                <input type="date" />
+              <div className="flex flex-col items-center w-full">
+                <p className="text-xl">Date you can come to visit.</p>
+                <input className="w-[80%]" type="date" />
               </div>
             </div>
-            <div className="flex flex-col">
-              <label>6 AM - 9 AM</label>
-              <input type="checkbox" />
-              <label>9 AM - Noon</label>
-              <input type="checkbox" />
-              <label>Noon - 4 PM</label>
-              <input type="checkbox" />
-              <label>4 PM - 7 PM</label>
-              <input type="checkbox" />
-              <label>7 PM - 10 PM</label>
-              <input type="checkbox" />
+            <div className="flex flex-col items-center md:w-1/2">
+              <p className="text-xl">Times of day you can come visit.</p>
+              <div className='flex flex-col w-[70%]'>
+                <div className="form-checkbox-div">
+                  <label>6 AM - 9 AM</label>
+                  <input type="checkbox" />
+                </div>
+                <div className="form-checkbox-div">
+                  <label>9 AM - Noon</label>
+                  <input type="checkbox" />
+                </div>
+                <div className="form-checkbox-div">
+                  <label>Noon - 4 PM</label>
+                  <input type="checkbox" />
+                </div>
+                <div className="form-checkbox-div">
+                  <label>4 PM - 7 PM</label>
+                  <input type="checkbox" />
+                </div>
+                <div className="form-checkbox-div">
+                  <label>7 PM - 10 PM</label>
+                  <input type="checkbox" />
+                </div>
+              </div>
             </div>
           </div>
-          <button type="submit" className="btn md:w-1/2 lg:w-1/3">
+          <button type="submit" className="btn md:w-1/2 lg:w-1/3 md:mt-2">
             Submit
           </button>
         </form>
