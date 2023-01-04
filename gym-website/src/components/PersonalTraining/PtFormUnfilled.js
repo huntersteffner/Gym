1b16473079aa843d34897d9cbc386ea1d211f5e5
huntersteffner <huntersteffner@gmail.com>
2023-01-04 10:56:53 -0500

    PT Form Styling

--- gym-website/src/components/PersonalTraining/PtFormUnfilled.js
+++ gym-website/src/components/PersonalTraining/PtFormUnfilled.js
@@ -1,73 +1,85 @@
 import Button from '../Button'
 
 const PtFormUnfilled = () => {
-    return (
-        <div className="m-2">
-          <div>
-            <form className="flex flex-col justify-center items-center">
-              <h3 className="text-center text-5xl">
-                Sign Up Today for a Free Personal Training Session
-              </h3>
-              <div className="flex flex-col md:flex-row space-x-10">
-                <div className="flex flex-col">
-                  <h4 className="text-lg">Enter your name and email</h4>
-                  <div className="flex flex-col justify-evenly h-full w-full">
-                    <label>Name</label>
-                    <input type="text" placeholder="Name" />
-                    <label>Email</label>
-                    <input type="email" id="email" placeholder="Email" />
-                  </div>
+  return (
+    <div className="bg-amber-800 text-white">
+      <form className="flex flex-col justify-center items-center p-2">
+        <h3 className="text-3xl text-center md:text-5xl">
+          Sign Up Today for a Free Personal Training Session
+        </h3>
+        <div className="flex flex-col md:flex-row md:space-x-10">
+          <div className="flex flex-col ">
+            <h4 className="text-lg text-center ">Enter your name and email</h4>
+            <div className="flex flex-col justify-evenly h-full w-full space-y-2 md:space-y-0">
+              <input type="text" placeholder="Name" />
+              <input type="email" id="email" placeholder="Email" />
+            </div>
+          </div>
+          <div className='flex flex-col items-center md:w-1/2'>
+            <h4 className="text-lg">
+              What types of training are you interested in?
+            </h4>
+            <div className="flex flex-col w-[70%]">
+              <div className='form-checkbox-div'>
+                <label>Cardio</label>
+                <input type="checkbox" />
+              </div>
+              <div className='form-checkbox-div'>
+                <label>Nutrition</label>
+                <input type="checkbox" />
+              </div>
+              <div className='form-checkbox-div'>
+                <label>Physical Therapy</label>
+                <input type="checkbox" />
+              </div>
+              <div className='form-checkbox-div'>
+                <label>Strength Training</label>
+                <input type="checkbox" />
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col items-center w-full'>
+            <h4
+              
+              className="text-lg"
+            >
+              Please select a tentative date and times of day that work best for
+              you.
+            </h4>
+            <input className='w-[80%]' type="date" />
+            <div className='flex flex-col w-[70%]'>
+                <div className="form-checkbox-div">
+                  <label>6 AM - 9 AM</label>
+                  <input type="checkbox" />
                 </div>
-                <div>
-                  <h4 className="text-lg">
-                    What types of training are you interested in?
-                  </h4>
-                  <div className="flex flex-col">
-                    <label>Cardio</label>
-                    <input type="checkbox" />
-                    <label>Nutrition</label>
-                    <input type="checkbox" />
-                    <label>Physical Therapy</label>
-                    <input type="checkbox" />
-                    <label>Strength Training</label>
-                    <input type="checkbox" />
-                  </div>
+                <div className="form-checkbox-div">
+                  <label>9 AM - Noon</label>
+                  <input type="checkbox" />
                 </div>
-                <div>
-                  <h4
-                    onClick={() => {
-                      console.log('It worked')
-                    }}
-                    className="text-lg"
-                  >
-                    Please select a tentative date and times of day that work best
-                    for you.
-                  </h4>
-                  <label>Date</label>
-                  <input type="date" />
-                  <div className="flex flex-col">
-                    <label>6 AM - 9 AM</label>
-                    <input type="checkbox" />
-                    <label>9 AM - Noon</label>
-                    <input type="checkbox" />
-                    <label>Noon - 4 PM</label>
-                    <input type="checkbox" />
-                    <label>4 PM - 7 PM</label>
-                    <input type="checkbox" />
-                    <label>7 PM - 10 PM</label>
-                    <input type="checkbox" />
-                  </div>
+                <div className="form-checkbox-div">
+                  <label>Noon - 4 PM</label>
+                  <input type="checkbox" />
+                </div>
+                <div className="form-checkbox-div">
+                  <label>4 PM - 7 PM</label>
+                  <input type="checkbox" />
+                </div>
+                <div className="form-checkbox-div">
+                  <label>7 PM - 10 PM</label>
+                  <input type="checkbox" />
                 </div>
               </div>
-              <Button
-                onSubmit={() => {
-                  console.log('It worked')
-                }}
-              />
-            </form>
           </div>
         </div>
-      )
+        {/* <Button
+          onSubmit={() => {
+            console.log('It worked')
+          }}
+        /> */}
+        <button className="btn md:w-1/2 lg:w-1/3">Submit</button>
+      </form>
+    </div>
+  )
 }
 
 export default PtFormUnfilled
